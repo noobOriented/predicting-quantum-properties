3559abd723fb2a044f2e5f34ea74a328261561dc
noobOriented <jsaon92@gmail.com>
2024-11-10 22:26:46 +0800

    refactor prediction_shadow

--- prediction_shadow.py
+++ prediction_shadow.py
@@ -1,6 +1,49 @@
 import sys
 
-def estimate_exp(full_measurement, one_observable):
+
+def main():
+    def print_usage():
+        print('Usage:\n', file=sys.stderr)
+        print('./prediction_shadow -o [measurement.txt] [observable.txt]', file=sys.stderr)
+        print('    This option predicts the expectation of local observables.', file=sys.stderr)
+        print('    We would output the predicted value for each local observable given in [observable.txt]', file=sys.stderr)
+
+    if len(sys.argv) != 4:
+        print_usage()
+        exit()
+
+    if sys.argv[1] != '-o':
+        raise NotImplementedError
+
+    with open(sys.argv[2]) as f:
+        system_size = int(f.readline())
+        measurements = f.readlines()
+
+    full_measurement = [
+        [
+            (pauli_XYZ, int(outcome))
+            for pauli_XYZ, outcome in zip(line.split(' ')[0::2], line.split(' ')[1::2])
+        ]
+        for line in measurements
+    ]
+
+    with open(sys.argv[3]) as f:
+        observable_size = int(f.readline())
+        observables = f.readlines()
+
+    for line in observables:
+        one_observable = [
+            (pauli_XYZ, int(position))
+            for pauli_XYZ, position in zip(line.split(' ')[1::2], line.split(' ')[2::2])
+        ]
+        sum_product, cnt_match = estimate_exp(full_measurement, one_observable)
+        print(sum_product / cnt_match)
+
+
+def estimate_exp(
+    full_measurement: list[list[tuple[str, int]]],
+    one_observable: list[tuple[str, int]],
+) -> tuple[int, int]:
     sum_product, cnt_match = 0, 0
 
     for single_measurement in full_measurement:
@@ -12,41 +55,14 @@ def estimate_exp(full_measurement, one_observable):
                 not_match = 1
                 break
             product *= single_measurement[position][1]
-        if not_match == 1: continue
+        if not_match == 1:
+            continue
 
         sum_product += product
         cnt_match += 1
 
     return sum_product, cnt_match
 
-if __name__ == "__main__":
-    def print_usage():
-        print("Usage:\n", file=sys.stderr)
-        print("./prediction_shadow -o [measurement.txt] [observable.txt]", file=sys.stderr)
-        print("    This option predicts the expectation of local observables.", file=sys.stderr)
-        print("    We would output the predicted value for each local observable given in [observable.txt]", file=sys.stderr)
 
-    if len(sys.argv) != 4:
-        print_usage()
-
-    with open(sys.argv[2]) as f:
-        measurements = f.readlines()
-    system_size = int(measurements[0])
-
-    full_measurement = []
-    for line in measurements[1:]:
-        single_meaurement = []
-        for pauli_XYZ, outcome in zip(line.split(" ")[0::2], line.split(" ")[1::2]):
-            single_meaurement.append((pauli_XYZ, int(outcome)))
-        full_measurement.append(single_meaurement)
-
-    with open(sys.argv[3]) as f:
-        content = f.readlines()
-    assert(system_size == int(content[0]))
-
-    for line in content[1:]:
-        one_observable = []
-        for pauli_XYZ, position in zip(line.split(" ")[1::2], line.split(" ")[2::2]):
-            one_observable.append((pauli_XYZ, int(position)))
-        sum_product, cnt_match = estimate_exp(full_measurement, one_observable)
-        print(sum_product / cnt_match)
+if __name__ == '__main__':
+    main()
