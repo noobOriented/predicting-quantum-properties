aa7e06d1c4acc897dfbfea030dbf4e497640f7f6
noobOriented <jsaon92@gmail.com>
2024-11-11 00:34:38 +0800

    add test for data_acquisition_shadow

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -1,8 +1,14 @@
+import contextlib
+import io
 import pathlib
+import runpy
+import sys
+from unittest.mock import patch
 
 import pytest
 from typer.testing import CliRunner
 
+import data_acquisition_shadow
 import generate_observables
 import prediction_shadow
 
@@ -19,6 +25,28 @@ def test_prediction_shadow():
 
     expected_result = open(SNAPSHOT_PATH / 'prediction_shadow.txt').read()
     assert result.stdout == expected_result
+
+
+@pytest.mark.parametrize('measurements_per_observable, observable_path, expected_result', [
+    (
+        10,
+        SNAPSHOT_PATH / 'generated_observables_5.txt',
+        SNAPSHOT_PATH / 'data_acquisition_shadow_d_m10_system_size5.txt',
+    ),
+])
+def test_data_acquisition_shadow(
+    measurements_per_observable,
+    observable_path,
+    expected_result,
+):
+    with (
+        patch.object(sys, 'argv', [data_acquisition_shadow.__name__, '-d', str(measurements_per_observable), str(observable_path)]),
+        contextlib.redirect_stdout(io.StringIO()) as stream,
+    ):
+        runpy.run_module(data_acquisition_shadow.__name__, run_name='__main__')
+
+    expected_result = open(expected_result).read()
+    assert stream.getvalue() == expected_result
 
 
 @pytest.mark.parametrize('system_size, expected_result', [
